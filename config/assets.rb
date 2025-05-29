4a87b4bfbc4160b39b57884693df183e4d792c0c
DanieleNoto <daniele.noto@outlook.com>
2025-05-29 14:15:34 +0200

    bugs fixed

--- config/assets.rb
+++ config/assets.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
-# Calcoliamo il percorso base per il modulo
-base_path = "/home/daniele/decidim0.29.1-idra/decidim-module-idra"
+base_path = File.expand_path("..", __dir__)
 
-# Registriamo il percorso dei pacchetti e forziamo il caricamento per primo
 Decidim::Webpacker.register_path("#{base_path}/app/packs", prepend: true)
 
-# Registriamo anche gli entrypoints specifici per il modulo
 Decidim::Webpacker.register_entrypoints(
-  decidim_iframe: "#{base_path}/app/packs/entrypoints/decidim_iframe.scss"
+  decidim_idra_editor: "#{base_path}/app/packs/entrypoints/decidim_idra_editor.js"
 )
--- lib/decidim/idra/engine.rb
+++ lib/decidim/idra/engine.rb
@@ -5,13 +5,11 @@ require "decidim/core"
 
 module Decidim
   module Idra
-    # This is the engine that runs on the public interface of idra.
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::Idra
 
       initializer "idra.add_routes" do |app|
         app.routes.append do
-          # Aggiorna i percorsi delle rotte per non includere il namespace "idra"
           get "/idra", to: "idra#index"
           post "/idra_create", to: "idra#create"
           get "/idra_update", to: "idra#update"
@@ -21,12 +19,13 @@ module Decidim
         end
       end
 
-
       initializer "decidim_idra.add_cells_view_paths" do
-        Cell::ViewModel.view_paths.unshift(File.expand_path("#{Decidim::Idra::Engine.root}/app/cells"))
+        Cell::ViewModel.view_paths.unshift(
+          File.expand_path("#{Decidim::Idra::Engine.root}/app/cells")
+        )
       end
 
-
+      
     end
   end
 end
