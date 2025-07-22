84f613ecbf1c73a03ff155ec4bcd923d4b6c076a
DanieleNoto <daniele.noto@outlook.com>
2025-07-22 12:21:41 +0200

    problem double Idra solved

--- lib/decidim/idra/engine.rb
+++ lib/decidim/idra/engine.rb
@@ -25,6 +25,17 @@ module Decidim
         )
       end
 
+      idra_url = "/idra"
+      idra="Idra"
+
+      Decidim.menu :menu do |menu|
+          menu.add_item :root,
+              idra,
+              idra_url,
+              position: 6,
+              active: :exclusive
+      end
+
       
     end
   end
